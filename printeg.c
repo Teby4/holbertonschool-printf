2b3d5932abfd7657518affe217fa4a14a4b8eaf5
Nacho <146840051+riveroignacio04@users.noreply.github.com>
2023-11-16 11:36:45 -0300

    print numbers for %d and %i

---
+++ printeg.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * printeg - imprime un número entero
+ * @num: número a imprimir
+ * @count: puntero al contador de caracteres impresos
+ *
+ * Return: no devuelve nada
+ */
+void printeg(int num, int *count)
+{
+	if (num == -2147483648)
+	{
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
+		(*count) += 10;
+		return;
+	}
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		(*count)++;
+	}
+
+	if (num / 10 != 0)
+	{
+		printeg(num / 10, count);
+	}
+
+	_putchar((num % 10) + '0');
+	(*count)++;
+}
